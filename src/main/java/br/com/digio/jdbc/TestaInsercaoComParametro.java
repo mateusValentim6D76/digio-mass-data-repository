7c24aa7f235924c68ffb387315a0537cd00205f7
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-07-16 21:13:38 -0300

    Controle de transacao: setAutoCommit = false

--- src/main/java/br/com/digio/jdbc/TestaInsercaoComParametro.java
+++ src/main/java/br/com/digio/jdbc/TestaInsercaoComParametro.java
@@ -9,17 +9,32 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		String nome = "TESTE 2'";
-		String descricao = "TESTE 2); DELETE FROM Produto;";
+
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection conn = connectionFactory.getConnection();
+		conn.setAutoCommit(false);
 
-		PreparedStatement stm = conn.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
-				Statement.RETURN_GENERATED_KEYS);
+		try {
+			PreparedStatement stm = conn.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 
+			adicionarVariavel("Smart TV", "80 Polegadas", stm);
+			adicionarVariavel("Radio", "Radio de pilha", stm);
+			conn.commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ROLLBACK EXECUTADO");
+			conn.rollback();
+		}
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-
+//		if(nome.equals("Radio")) {
+//			throw new RuntimeException("Nao foi possivel adicionar o produto");
+//		}
 		System.out.println(stm);
 		stm.execute();
 		ResultSet rst = stm.getGeneratedKeys();
